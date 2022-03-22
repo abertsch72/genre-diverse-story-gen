27e540e2292889e4a01a62cfab27fcaf138a530b
James Hill <47001731+JamesHill0@users.noreply.github.com>
2022-03-22 09:10:04 -0400

    Prompt Loading Script Written
    Script loads prompts from dataset found here: https://www.kaggle.com/datasets/ratthachat/writing-prompts

--- load_data.py
+++ load_data.py
@@ -1,12 +1,28 @@
-from typing import Text, Dict
+# Credit to: https://www.kaggle.com/code/ratthachat/writingprompts-combine-one-line-data-for-gpt2/notebook
 
 
-def load_prompts() -> (Dict[Text: Text], Dict[Text: Text]):
-    # prompt and story values should both be lists
-    train_data = {"prompt": None, "story": None}
-    val_data = {"prompt": None, "story": None}
+def load_prompts(num_words = None, dir = "../writingPrompts/"):
 
-    assert len(train_data["prompt"]) == len(train_data["story"])
-    assert len(val_data["prompt"]) == len(val_data["story"])
+    # num_words: The number of words to be used from each story (when None is passed full stories are returned)
+    # dir: Directory of folder containing prompts
+    
+    directories = ["train", "test", "valid"]
+    data = dict()
 
-    return train_data, val_data
+    for name_id in range(len(directories)):
+
+        fp = open(dir + directories[name_id] + ".wp_source") 
+        ft = open(dir + directories[name_id] + ".wp_target") 
+
+        
+        prompts = fp.readlines()
+        stories = ft.readlines()
+
+        assert(len(stories) == len(prompts))
+
+        if (num_words != None):
+            stories = [prompts[i].rstrip()+ " <endprompts> " + " ".join(stories[i].split()[0:num_words]) for i in range(len(stories))]
+
+        data[directories[name_id]] = {"prompt": prompts, "story": stories}
+        
+    return data["train"], data["test"], data["valid"]
