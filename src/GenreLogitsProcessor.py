70a12f1ab1d7a88748883a36b991fccc5db2ac04
xinyiluo448 <65972761+xinyiluo448@users.noreply.github.com>
2022-04-29 12:34:51 -0400

    added code for init

--- src/GenreLogitsProcessor.py
+++ src/GenreLogitsProcessor.py
@@ -1,7 +1,8 @@
 from transformers.generation_logits_process import LogitsProcessor
 import torch
-
+import numpy as np
 from typing import Text
+
 
 class InfluenceGenreLogitsProcessor(LogitsProcessor):
     r"""
@@ -13,7 +14,11 @@ class InfluenceGenreLogitsProcessor(LogitsProcessor):
     """
 
     def __init__(self, genre: Text, method: Text = "MAP", lambda_val: float = 0.1):
-        valid_genres = [] # TODO: add a list of valid genres here
+        count_matrix = np.load("./../count-matrix-files/count-matrix.npy")
+        total_genre_counts = np.load("./../count-matrix-files/total-genre-counts.npy")
+        bookcorp_count = 
+        bookcorp_total_count =
+        valid_genres = ["Romance", "Fantacy"] 
         valid_methods = ["MLE", "MAP"]
         if genre not in valid_genres:
             raise ValueError(f"Genre {genre} is not in list of valid genres: {valid_genres}")
@@ -21,7 +26,15 @@ class InfluenceGenreLogitsProcessor(LogitsProcessor):
             raise ValueError(f"Method {method} is not in list of valid methods: {valid_methods}")
 
         self.genre = genre
-        self.probabilities = None # TODO: load in the correct list of probabilities, based on genre and method
+        self.genre_index = valid_genres.index(self.genre)
+        if method == "MLE":
+            self.probabilities = count_matrix[:, self.genre_index]/total_genre_counts[self.genre_index] # TODO: load in the correct list of probabilities, based on genre and method
+        else: #MAP
+            alpha = # count of tok1  -- orig bookcorp
+            beta = # total token count - alpha   -- orig bookcorp
+            self.probabilities = (count_matrix[:, self.genre_index] + alpha - np.ones((50257, 1))/
+                                total_genre_counts[self.genre_index] + alpha + beta - np.full((50257, 1), 2))
+
         self.lambda_val = lambda_val #TODO: is the default I've provided (0.1) the best value?
 
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor) -> torch.FloatTensor:
